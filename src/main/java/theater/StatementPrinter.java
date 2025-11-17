3fcb90d5c1b834c8c5a20bdfb3449f243e090b1e
zaynahahmer <zaynah.ahmer@mail.utoronto.ca>
2025-11-17 01:36:36 -0500

    Task 2.4: Removing Total Volume Credits and Total Amount

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -23,24 +23,34 @@ public class StatementPrinter {
      */
 
     public String statement() {
-        int totalAmount = 0;
-        int volumeCredits = 0;
 
         final StringBuilder result = new StringBuilder("Statement for " + invoice.getCustomer()
                 + System.lineSeparator());
-
         for (Performance performance : invoice.getPerformances()) {
-
-            volumeCredits += getVolumeCredits(performance, volumeCredits);
-
             // print line for this order
             result.append(String.format("  %s: %s (%s seats)%n", getPlay(performance).getName(),
                     usd(getAmount(performance)), performance.getAudience()));
-            totalAmount += getAmount(performance);
         }
-        result.append(String.format("Amount owed is %s%n", usd(totalAmount)));
-        result.append(String.format("You earned %s credits%n", volumeCredits));
+
+        result.append(String.format("Amount owed is %s%n", usd(getTotalAmount())));
+        result.append(String.format("You earned %s credits%n", getTotalVolumeCredits()));
         return result.toString();
+    }
+
+    private int getTotalAmount() {
+        int result = 0;
+        for (Performance performance : invoice.getPerformances()) {
+            result += getAmount(performance);
+        }
+        return result;
+    }
+
+    private int getTotalVolumeCredits() {
+        int result = 0;
+        for (Performance performance : invoice.getPerformances()) {
+            result += getVolumeCredits(performance, result);
+        }
+        return result;
     }
 
     private static String usd(int totalAmount) {
